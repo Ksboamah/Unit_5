069d2ab27cf1893f626f88fa717207bcb183ed6b
Kwame <bkwamesakyi@gmail.com>
2019-10-25 10:17:02 -0400

    I started my Unit 5 daily exercises. I worked on my daily exercises. I worked on my assignment. I finished my unit 5 assignment. I almost finished my unit 5 daily exercises.

--- daily_exercises.py
+++ daily_exercises.py
@@ -1,3 +1,6 @@
+import random
+
+
 def choose_upper_value():
     small = int(input("Count from (Pick a number)"))
     return small
@@ -22,9 +25,6 @@ def counter(small, big):
 def multiply():
     num = int(input("Pick an integer"))
     return num
-    for x in range(13):
-        print(num) * print(y)
-
 
 
 def main():
@@ -36,9 +36,33 @@ def main():
     #     print(x)
     # for x in range(2, 41, 2):
     #     print(x)
-    y == -1
-    y =
-    multiply()
+    # y = -1
+    # y += 1
+    # num = multiply()
+    # for x in range(13):
+    #     print(num, "*", y, "=", num * y)
+    #     y += 1
+    # even = 0
+    # odd = 0
+    # for x in range(10):
+    #     rando = random.randint(1, 100)
+    #     if rando % 2 == 0:
+    #         even = even + rando
+    #     if rando % 2 == 1:
+    #         odd = odd + rando
+    # print(odd)
+    # print(even)
+    # three = 0
+    # five = 0
+    # for x in range(1000):
+    #     if x % 3 == 0:
+    #         three = three + x
+    #     if x % 5 == 0:
+    #         five = five + x
+    # print(three + five)
+    size = int(input("How big do you want your triangle to be? (Enter in integer)"))
+    for x in range(size):
+        print("* " * (x + 1))
 
 
 main()
